0f801768d4a0ba87c52878fcd0c7cc248121e3d0
ma-xima <maxima.alk@hak-reutte.ac.at>
2024-01-15 10:31:16 +0100

    new package with 2 classes

---
+++ src/TestCode2/Klasse1.java
@@ -0,0 +1,12 @@
+package TestCode2;
+
+public class Klasse1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Commit Message Test");
+		System.out.println("local commit test");
+
+	}
+
+}
---
+++ src/TestCode2/Klasse2.java
@@ -0,0 +1,13 @@
+package TestCode2;
+
+public class Klasse2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello GitHub!");
+		System.out.println("Klasse 2");
+		System.out.println("local commit test end");
+
+	}
+
+}
