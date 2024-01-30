ba11d5e26131ebde6148a17cbfb1702d4d8c5024
ma-xima <maxima.alk@hak-reutte.ac.at>
2024-01-30 09:45:35 +0100

    Ende Stunde

--- src/geoMetry/Test.java
+++ src/geoMetry/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		Circle c1 = new Circle (    );
+		Circle c1 = new Circle (  );
 		System.out.println(c1);
 		System.out.println(c1.calculateCircumference());
 		
