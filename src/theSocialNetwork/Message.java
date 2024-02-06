fa1fc9a9f149799e0431c42318f445f0017fe0e8
ma-xima <maxima.alk@hak-reutte.ac.at>
2024-02-06 08:35:20 +0100

    generated class User

--- src/theSocialNetwork/Message.java
+++ src/theSocialNetwork/Message.java
@@ -1,5 +1,17 @@
 package theSocialNetwork;
 
-public class Message {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class Message extends Post{
+
+	private String text;
+	private ArrayList<Message> messages;
+	
+	public Message(User author, String text) {
+		super();
+		this.setAuthor(author);
+		this.messages = new ArrayList<Message>();
+		this.text = text;
+	}
 }
--- src/theSocialNetwork/User.java
+++ src/theSocialNetwork/User.java
@@ -1,5 +1,24 @@
 package theSocialNetwork;
 
 public class User {
+	
+	private String userName;
+	
+	public User(String userName) {
+	super();
+	this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String toString() {
+		return userName;
+	}
 
 }
