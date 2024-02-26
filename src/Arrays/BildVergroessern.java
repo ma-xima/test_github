8532966d7218fbd733b6b11c1fc6e44cf8d05b5f
ma-xima <maxima.alk@hak-reutte.ac.at>
2024-02-26 11:45:33 +0100

    stand schule

---
+++ src/Arrays/BildVergroessern.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+// Zeilenanzahl = matrix.length
+//Spaltenanzahl = matrix[0].length
+public class BildVergroessern {
+	int factor = 2;
+	static int [][] matrix = {{1,2,3}, {4,5,6}};
+	static int [][] b = new int [4][6];
+	public static int [][] magnify(int [][] array, int factor){
+		for(int i = 0; i< matrix.length; i++) {
+			for(int j = 0; j <matrix[0].length; j++) {
+				
+			}
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/Arrays/Umsatzsteigerung.java
+++ src/Arrays/Umsatzsteigerung.java
@@ -7,7 +7,7 @@ public class Umsatzsteigerung {
 	 private static int percentJumpCount;
 	 private static int[] dailyGains = new int[31];
 	 
-	 int count5PercentJumps(int[]gains) {
+	 public static int count5PercentJumps(int[]gains) {
 		 for(int i = 0; i <= dailyGains.length; i++) {
 			 day = dailyGains[i];
 			 if(i >= dailyGains.length) {
