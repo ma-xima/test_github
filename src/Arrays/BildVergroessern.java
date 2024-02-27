72bb4d294ac4cc33fcfbe4e1b7d6edf03177d54b
ma-xima <maxima.alk@hak-reutte.ac.at>
2024-02-27 08:09:32 +0100

    richtig

--- src/Arrays/BildVergroessern.java
+++ src/Arrays/BildVergroessern.java
@@ -1,23 +1,47 @@
 package Arrays;
 
-// Zeilenanzahl = matrix.length
-//Spaltenanzahl = matrix[0].length
-public class BildVergroessern {
-	int factor = 2;
-	static int [][] matrix = {{1,2,3}, {4,5,6}};
-	static int [][] b = new int [4][6];
-	public static int [][] magnify(int [][] array, int factor){
-		for(int i = 0; i< matrix.length; i++) {
-			for(int j = 0; j <matrix[0].length; j++) {
-				
-			}
-		}
-		
-		
-	}
-	
-	public static void main(String[] args) {
-		
-	}
+import java.util.concurrent.ThreadLocalRandom;
 
+public class BildVergroessern {
+
+  //tag::solution[]
+  public static int[][] magnify( int[][] array, int factor ) {
+    int width = array[ 0 ].length;
+    int height = array.length;
+    int[][] result = new int[ height * factor ][ width * factor ];
+
+    for ( int row = 0; row < result.length; row++ ) {
+      int[] cols = result[ row ];
+      for ( int col = 0; col < cols.length; col++ )
+        cols[ col ] = array[ row / factor ][ col / factor ];
+    }
+    return result;
+  }
+
+  private static void printValues( int[][] array ) {
+    for ( int[] rows : array ) {
+      for ( int col = 0; col < rows.length; col++ )
+        System.out.printf( "%03d%s",
+                           rows[ col ], col == rows.length - 1 ? "" : ", " );
+      System.out.println();
+    }
+  }
+
+  private static void fillWithRandomValues( int[][] array ) {
+    for ( int row = 0; row < array.length; row++ ) {
+      int[] cols = array[ row ];
+      for ( int col = 0; col < cols.length; col++ ) {
+        array[ row ][ col ] = ThreadLocalRandom.current().nextInt( 256 );
+      }
+    }
+  }
+
+  public static void main( String[] args ) {
+    int[][] testArray = new int[ 2 ][ 5 ];
+    fillWithRandomValues( testArray );
+    printValues( testArray );
+    int[][] result = magnify( testArray, 2 );
+    printValues( result );
+  }
+  //end::solution[]
 }
