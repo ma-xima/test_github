257db2db7d2d7160bab982a6581459cf587cea8c
ma-xima <maxima.alk@hak-reutte.ac.at>
2024-03-05 09:45:37 +0100

    Musical chairs fertig

--- src/musicalChairs/MusicalChairsGame.java
+++ src/musicalChairs/MusicalChairsGame.java
@@ -3,6 +3,20 @@ package musicalChairs;
 public class MusicalChairsGame {
 	public static void main(String[] args) {
 		
+		MusicalChair mc1 = new MusicalChair("Lisa", "Susi", "Tom", "Lukas");
+		System.out.println(mc1);
+		
+		MusicalChair mc2 = new MusicalChair("Madeline", "Benedikt");
+		System.out.println(mc2);
+		
+		//MusicalChair mc3 = new MusicalChair();
+		//System.out.println(mc3);
+		
+		mc1.rotate(2);
+		System.out.println(mc1);
+		
+		mc1.rotateAndRemove(2);
+		System.out.println(mc1);
 	}
 
 }
