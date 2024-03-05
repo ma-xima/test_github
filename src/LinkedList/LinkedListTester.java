25702bb8a9faef52df47ca355be80b4a7c979273
ma-xima <maxima.alk@hak-reutte.ac.at>
2024-03-05 08:43:56 +0100

    interaator test

---
+++ src/LinkedList/LinkedListTester.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListTester {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = new LinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		
+		//"Laufvariable" zum Iterieren einer Datenstruktur
+		//läuft immer vom Anfang bis zum Ende
+		Iterator<Integer> iterator = list.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+
+	}
+
+}
