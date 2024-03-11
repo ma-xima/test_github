d6c04d64c3d76cd14b52465731f93329b13f9ee9
Maxima <Maxima@172.20.3.58>
2024-03-11 11:44:51 +0100

    Schule

---
+++ src/LinkedList/CheeseInserter.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.regex.Pattern;
+
+public class CheeseInserter {
+	
+	private static final Pattern vegetables = Pattern.compile(
+			"Zucchini|Paprikas?|Zwiebeln?|Tomaten?");
+	
+	public static void insertCheeseAroundVegetable(List<String>ingredients) {
+		//Initialisierung Laufvariable (iterator)
+		// Abbruchbedingung (hasNext())
+		// keine Inkrementierung -> passiert im Schleifenkörper
+		for(ListIterator<String> iterator = ingredients.listIterator(); iterator.hasNext();) {
+			String ingredient = iterator.next();
+		}
+	}
+			
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
