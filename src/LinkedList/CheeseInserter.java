7ba379df3e9b954f009e96ed50efdfdcdb340aea
Maxima <Maxima@LAPTOP-AL5LTPTD>
2024-03-12 08:31:43 +0100

    fertig

--- src/LinkedList/CheeseInserter.java
+++ src/LinkedList/CheeseInserter.java
@@ -1,13 +1,19 @@
 package LinkedList;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.regex.Pattern;
 
 public class CheeseInserter {
 	
+	//private - Sichtbarkeitsbereich auf Klasse beschränkt
+		//static - von der Klasse abhängig und nicht vom Objekt
+		//final - variable nach Initialisierung unveränderbar
 	private static final Pattern vegetables = Pattern.compile(
 			"Zucchini|Paprikas?|Zwiebeln?|Tomaten?");
+	
 	
 	public static void insertCheeseAroundVegetable(List<String>ingredients) {
 		//Initialisierung Laufvariable (iterator)
@@ -15,12 +21,18 @@ public class CheeseInserter {
 		// keine Inkrementierung -> passiert im Schleifenkörper
 		for(ListIterator<String> iterator = ingredients.listIterator(); iterator.hasNext();) {
 			String ingredient = iterator.next();
+			if(vegetables.matcher(ingredient).matches()) {
+				iterator.add("Käse");
+			}
 		}
 	}
 			
 
 	public static void main(String[] args) {
-		
+		List<String> ingredients = new LinkedList<String>();
+		Collections.addAll(ingredients,"Gnocchi", "Paprika", "Tomate", "Pfeffer");
+		insertCheeseAroundVegetable(ingredients);
+		System.out.println(ingredients);
 		
 
 	}
