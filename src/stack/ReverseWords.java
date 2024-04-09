c4b172a66b7f63acd06decd101c8c32970513f5f
Maxima <Maxima@LAPTOP-AL5LTPTD>
2024-04-09 08:31:37 +0200

    Fertig Aufagbe 1

--- src/stack/ReverseWords.java
+++ src/stack/ReverseWords.java
@@ -1,14 +1,50 @@
 package stack;
 
-public class ReverseWords {
-	
-	reverseString(String input){
-		
+import java.util.Stack;
+
+public class ReverseWords {  
+	public static String reverseString(String input) {
+		if (input == null)
+        return null;
+
+    Stack<Character> stack = new Stack<>();
+    // Den String in den Stack umkehren
+    	for (int i = 0; i < input.length(); i++) {
+        stack.push(input.charAt(i));
+    }
+
+    // Den umgekehrten String zusammensetzen
+    StringBuilder reversed = new StringBuilder();
+    	while (!stack.isEmpty()) {
+    		reversed.append(stack.pop());
+    	}
+
+    	return reversed.toString();
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "Hallo Welt!";
+		System.out.println("Eingabe: " + input);
+		System.out.println("Umgekehrter String: " + reverseString(input));
+    
 	}
-
 }
+
+	/*private int front;
+	private int capacity;
+	private int[] stack;
+	private String input;
+	
+	
+	public reverseString(String input){
+		this.front = -1;
+		this.capacity = capacity;
+		this.stack = new int[capacity];
+		this.input = new String [input];
+	}
+	*/
+
+
+
+
+
