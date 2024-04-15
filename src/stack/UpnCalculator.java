bd84e84dd11ed0273e6fad2b8286d2b4be9d2b8f
Maxima <Maxima@LAPTOP-AL5LTPTD>
2024-04-15 10:39:06 +0200

    Code größtenteils fertig

---
+++ src/stack/UpnCalculator.java
@@ -0,0 +1,58 @@
+package stack;
+import java.util.Stack;
+import java.util.Scanner;
+
+public class UpnCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ausdruck in umgekehrter polnischenr Notation eingeben:");
+        String input = scanner.nextLine();
+        
+        try {
+            double result = evaluateUPN(input);
+            System.out.println("Ergebnis: " + result);
+        } catch (Exception e) {
+            System.out.println("Fehler beim Auswerten des Ausdrucks!");
+        }
+        
+        scanner.close();
+    }
+    
+    public static double evaluateUPN(String input) {
+        String[] tokens = input.split("\\s+");
+        Stack<Double> stack = new Stack<>();
+        
+        for (String token : tokens) {
+            if (token.matches("-?\\d+(\\.\\d+)?")) {
+                stack.push(Double.parseDouble(token));
+            } else if (token.equals("+")) {
+                if (stack.size() < 2) {
+                    System.out.println("Nicht genug Operanden für Addition");
+                    return 0; // Rückgabe eines Platzhalters
+                }
+                double b = stack.pop();
+                double a = stack.pop();
+                stack.push(a + b);
+            } else if (token.equals("*")) {
+                if (stack.size() < 2) {
+                    System.out.println("Nicht genug Operanden für Multiplikation");
+                    return 0; 
+                }
+                double b = stack.pop();
+                double a = stack.pop();
+                stack.push(a * b);
+            } else {
+                System.out.println("Ungültiges Token: " + token);
+                return 0; 
+            }
+        }
+        
+        if (stack.size() != 1) {
+            System.out.println("Ungültiger Ausdruck");
+            return 0; 
+        }
+        
+        return stack.pop();
+    }
+}
